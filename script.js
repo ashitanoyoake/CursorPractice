b2240c6b67f4411f5858c9f6ad591e6ca6a31e51
Fukuchi <fukuchi@MacBook-Pro.local>
2026-06-28 17:12:54 +0900

    ホームページ再構築（カーコーティングサイト）

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// 「自己紹介を見る」ボタンを取得
-const aboutButton = document.getElementById("about-button");
+// 「サービスを見る」ボタンを取得
+const serviceButton = document.getElementById("service-button");
 
-// 自己紹介セクションを取得
-const aboutSection = document.getElementById("about");
+// サービスセクションを取得
+const servicesSection = document.getElementById("services");
 
 // ボタンがクリックされたときの処理
-aboutButton.addEventListener("click", function () {
-  // 自己紹介セクションまでスクロールする
-  aboutSection.scrollIntoView({ behavior: "smooth" });
+serviceButton.addEventListener("click", function () {
+  // サービスセクションまでスクロールする
+  servicesSection.scrollIntoView({ behavior: "smooth" });
 });
